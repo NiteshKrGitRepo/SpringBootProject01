d78432d38640792ad7eb921c0c9410955301d213
KrNitesh <knitesh840.nk@gmail.com>
2025-01-28 23:21:21 +0530

    one line added in home controller

--- src/main/java/Com/nt/controller/ShowHomeController.java
+++ src/main/java/Com/nt/controller/ShowHomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ShowHomeController {
-
+//this is home controller
 	@RequestMapping("/home")
 	public String showHome() {
-		return"welcome";
+		return "welcome";
 	}
 }
